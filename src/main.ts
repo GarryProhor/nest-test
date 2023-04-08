7dace670289d5bfe79efb738d7922d78c92f7e4f
igorp <igorprohorchenko@gmail.com>
2023-04-08 09:28:06 +0300

    configuration swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('port');
+
+  const configSwagger = new DocumentBuilder()
+    .setTitle('Lesson api')
+    .setDescription('this api for lesson')
+    .setVersion('0.1')
+    .addTag('API')
+    .build();
+  const document = SwaggerModule.createDocument(app, configSwagger);
+  SwaggerModule.setup('api', app, document);
   await app.listen(port);
 }
 
