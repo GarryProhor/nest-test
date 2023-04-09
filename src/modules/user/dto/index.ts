666e09ed1598389b0f7880d3d204bc8bac6735e0
igorp <igorprohorchenko@gmail.com>
2023-04-09 15:16:09 +0300

    api schema request/response from openapi swagger

--- src/modules/user/dto/index.ts
+++ src/modules/user/dto/index.ts
@@ -1,16 +1,21 @@
 import { IsEmail, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UserDTO {
+  @ApiProperty()
   @IsString()
   firstName: string;
 
+  @ApiProperty()
   @IsString()
   userName: string;
 
+  @ApiProperty()
   @IsString()
   @IsEmail()
   email: string;
 
+  @ApiProperty()
   @IsString()
   password: string;
 }
