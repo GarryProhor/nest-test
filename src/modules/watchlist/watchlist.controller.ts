e1fed63a20fc1ee42e033b7226f6bfbdddf54795
igorp <igorprohorchenko@gmail.com>
2023-04-13 14:26:28 +0300

    create watch list endpoints

---
+++ src/modules/watchlist/watchlist.controller.ts
@@ -0,0 +1,41 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Patch,
+  Post,
+  Query,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { WatchlistService } from './watchlist.service';
+import { WatchListDTO } from './dto';
+import { JwtAuthGuard } from '../../guards/jwt-guard';
+
+@Controller('watchlist')
+export class WatchlistController {
+  constructor(private readonly watchList: WatchlistService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Post('/create')
+  createAsset(@Body() assetDto: WatchListDTO, @Req() request) {
+    const user = request.user;
+    return '';
+  }
+
+  @Get('/get-all')
+  getAllAssets() {
+    return;
+  }
+
+  @Patch('/update')
+  updateAsset() {
+    return;
+  }
+
+  @Delete('/:id')
+  deleteAsset(@Query('id') id: string) {
+    return;
+  }
+}
